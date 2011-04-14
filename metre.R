1ba329e19add0d7c501034bd7a29cc08cad77dc3
Andreas Jansson <andreas.s.t.jansson@gmail.com>
2011-04-15 00:27:38 +0100

    english spelling and added robustness

--- metre.R
+++ metre.R
@@ -1,5 +1,5 @@
 ##
-## RMidi Tools - Meter
+## RMidi Tools - Metre
 ## Copyright (C) 2011 Andreas Jansson
 ##
 ## This program is free software: you can redistribute it and/or modify
@@ -19,42 +19,60 @@
 
 library("rmidi")
 
-## Meter detection by autocorrelation of inter-onset intervals,
-## inspired by J. C. Brown "Determination of the meter of musical scores
+## Metre detection by autocorrelation of inter-onset intervals,
+## inspired by J. C. Brown "Detremination of the metre of musical scores
 ## by autocorrelation", 1993.
-extract.meter <- function(mat, min.subdiv = .25)
+## This function requires the quantise.R file to be sourced beforehand.
+extract.metre <- function(mat, min.subdiv = .25)
 {
   # quantise to min.subdiv (default == semiquavers)
   notes <- midi.to.notes(mat)
-  notes <- quantise(notes, min.subdiv, FALSE)
+  notes <- quantise.notes(notes, min.subdiv, FALSE)
 
   # build ioi sequence
   ioi.seq <- integer(max(notes[, "start"]) /
                      (midi.get.ppq() * min.subdiv) + 1)
   ioi.seq[notes[, "start"] / (midi.get.ppq() * min.subdiv) + 1] <-
-    notes[, "duration"]
+    notes[, "duration"] / (midi.get.ppq() * min.subdiv)
 
-  # get the autocorrelation
+  # get the autocorrelation 
   ioi.acf <- as.numeric(acf(ioi.seq, length(ioi.seq), plot = FALSE)$acf)
 
-  # find peaks using R port of S+ peaks function
-  peaks <- s.peaks(ioi.acf, 10)
-  peaks.indices <- which(peaks)
+  # if span is so big that less than two peaks are detected,
+  # gradually decrement the window.
+  # this rarely happens, but we want to cater for those
+  # corner cases as well.
+  peaks.span <- 3 / min.subdiv
+  repeat {
 
-  # find the most common distance between peaks
-  distances <- diff(peaks.indices)
+    # find peaks using R port of S+ peaks function
+    peaks <- s.peaks(ioi.acf, peaks.span)
+    peaks.indices <- which(peaks)
+
+    # find peak distances
+    distances <- diff(peaks.indices)
+
+    # if distances were found (i.e. more than 1 peak), exit loop
+    if(length(distances) > 0)
+      break;
+
+    # decrement span, possibly giving up
+    peaks.span <- floor(peaks.span / 2)
+    if(peaks.span < 2)
+      return(0)
+  }
+
+  # find most common distance
   distances.mode <- as.numeric(names(which.max(table(distances))))
 
-  # meter is the most common peak distance mutliplied by our
+  # metre is the most common peak distance mutliplied by our
   # minimum subdivision
-  meter <- distances.mode * min.subdiv
-  return(meter)
+  metre <- distances.mode * min.subdiv
+  return(metre)
 }
 
 # R port of S+ peaks function,
 # http://finzi.psych.upenn.edu/R/Rhelp02a/archive/33097.html
-# Brian Ripley (author) claims that span needs to be odd, but
-# oddly enough I found that span = 10 renders the best results.
 s.peaks<-function(series,span=3) 
 { 
   z <- embed(series, span) 
